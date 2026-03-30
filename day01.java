07a35ef739ac4ff706308e581dc2fcbf05b476e5
raghav-prshr <praghav1106@gmail.com>
2026-03-30 20:08:02 +0530

    Refactor day01 to accept user input for calculations

--- day01.java
+++ day01.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class day01 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
+        Scanner scn = new Scanner(System.in) ;
+        
+        System.out.print("Enter First Number: ");
+        int a = scn.nextInt();
+
+        System.out.print("Enter Second Number: ");
+        int b = scn.nextInt();
 
         System.out.println("Sum: " + (a + b));
         System.out.println("Product: " + (a * b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Float Division: " + ((float) a / b));
+        scn.close();
     }
 }
