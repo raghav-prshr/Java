74624a587c2c4353388c59701d440e40c3fd00aa
raghav-prshr <praghav1106@gmail.com>
2026-03-30 19:50:31 +0530

    Added Project Structure and Day01

---
+++ day01.java
@@ -0,0 +1,9 @@
+public class day01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Product: " + (a * b));
+    }
+}
